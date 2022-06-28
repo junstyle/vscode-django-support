9f5276b10c2043e90854f70a6b4ddcd4f0dcf995
junstyle <junstyle@git>
2022-06-28 15:38:22 +0800

    not format  style/script in comment regions

--- src/formatting.ts
+++ src/formatting.ts
@@ -88,8 +88,11 @@ function formatStyleAndScript(doc: { text: string }, options: Options) {
 		for (let i = 0; i < root.children.length; i++) {
 			const node = root.children[i];
 			if (node.type == 'element' && (node.name == 'script' || node.name == 'style')) {
-				if (node.prev && node.prev.value && node.prev.value.trim().endsWith('{# prettier-ignore #}')) {
-					continue;
+				if (node.prev && node.prev.value) {
+					let pv = node.prev.value.trim()
+					if (pv.endsWith('{# prettier-ignore #}') || pv.endsWith('{% comment %}')) {
+						continue;
+					}
 				}
 				let tagOffset = node.sourceSpan.start.offset;
 				let tagOffset2 = tagOffset;
